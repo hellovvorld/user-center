3c8f0eed9eaa683ba6e2d416b920458c174ff152
yuyongtao <smallbadoncelaugh@gmail.com>
2019-07-12 20:58:50 +0800

    add files !

--- src/main/java/com/itmuch/usercenter/UserCenterApplication.java
+++ src/main/java/com/itmuch/usercenter/UserCenterApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
+// 扫描mybatis哪些包里面的接口
 @MapperScan("com.itmuch")
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class UserCenterApplication {
 
     public static void main(String[] args) {
