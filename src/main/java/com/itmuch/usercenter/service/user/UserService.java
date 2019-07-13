e79915a789f2948d9ddf3f911ab5fe0568abb112
yuyongtao <smallbadoncelaugh@gmail.com>
2019-07-13 10:57:30 +0800

    add files !

--- src/main/java/com/itmuch/usercenter/service/user/UserService.java
+++ src/main/java/com/itmuch/usercenter/service/user/UserService.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * 根据Id查询用户信息
+ */
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserService {
